b377f4f2ce0cabe66d10e01abea8e4fae2322522
Gabriel Genaro <g.genaro.contato@gmail.com>
2024-09-08 00:41:11 -0300

    Teste 2

---
+++ src/testes_estágio/Teste2.java
@@ -0,0 +1,26 @@
+package testes_estágio;
+
+import java.util.Scanner;
+
+public class Teste2 {
+
+	public static void main(String[] args) {
+	      
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Informe um texto: ");
+        String texto = sc.nextLine();
+        
+
+        int cont = contLetraA(texto);
+        System.out.println("A letra 'a'  ocorre " + cont + " vezes no texto.");
+        sc.close();
+    }
+
+    public static int contLetraA(String str) {
+        return (int) str.chars().mapToObj(c -> (char) c).filter(c -> c == 'a' || c == 'A').count();
+    }
+
+	}
+
+
